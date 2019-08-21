2b13dc9ce7499dbc4c957f13fc3b132810cc3267
Dominique Skaf <doskaf@yahoo.com>
2019-08-21 23:37:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,8 @@ def monopoly_with_third_tier
         :three_pieces_owned => 100, 
         :four_pieces_owned => 200},
     :name => 
-        {:reading_railroad => {}, 
-        :pennsylvania_railroad => {},
+        {:reading_railroad => {:mortgage_value => $100}, 
+        :pennsylvania_railroad => {:mortgage},
         :b_and_o_railroad => {},
         :shortline_railroad => {}}
     }
